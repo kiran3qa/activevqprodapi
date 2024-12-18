b1bd8dc33b407b4710df61b9ef180e6e87286cac
kiran3qa <76578848+kiran3qa@users.noreply.github.com>
2024-12-18 22:42:05 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-const port = 3000
+const PORT = process.env.PORT || 3000
 const auth = require('./auth')
 
 app.use(express.json())
