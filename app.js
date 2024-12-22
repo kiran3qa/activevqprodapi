37a0d8aa6c8fdc7173bfca455b87ca11143b4fc8
kiran3qa <76578848+kiran3qa@users.noreply.github.com>
2024-12-22 19:08:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 
 const auth = require('./server/auth')
+const student = require('./server/addstudents')
 
 const PORT = 8000
+
+app.use(session({
+    secret : "hhfajshdjhdshfjdhsfjhajdhfdh",
+    resave : false,
+    saveUninitialized : true,
+    cookie : {secure : false}
+}));
 
 app.use(express.json())
 
 app.use('/users', auth)
+app.use('/student', student)
 
 app.listen(PORT, () => {
     console.log('server started')
