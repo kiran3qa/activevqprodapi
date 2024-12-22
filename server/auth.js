ab6b2771033028627ebce4893878655350009815
kiran3qa <76578848+kiran3qa@users.noreply.github.com>
2024-12-22 18:57:40 +0530

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -6,18 +6,17 @@ const connection = require('../dbconfig')
 const app = express();
 app.use(express.json())
 
-router.post('/login', (req, res) => {
+router.post('/login', async(req, res) => {
 
     const {email, passcode} = req.body;
     const ustatus = 1;
 
-    const query = connection.then(pool => {
+    await connection.then(pool => {
         return pool.request().query("SELECT urole FROM appusers WHERE email='"+email+"' AND passcode ='"+ passcode +"' AND ustatus = "+ ustatus+ "");
      }).then(result => {
-
-        res.json({success : true, response : result.recordset})
+            req.session.vqUserAuthCookie = email
+            res.json({authemail : req.session.vqUserAuthCookie ,success : true, response : result.recordset})
      })
-
 })
 
 module.exports = router
