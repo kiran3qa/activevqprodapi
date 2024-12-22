440b94e400eb6bba7849d00a79e804470de58d37
kiran3qa <76578848+kiran3qa@users.noreply.github.com>
2024-12-22 13:48:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
 
+const auth = require('./server/auth')
+
 const PORT = 8000
 
 app.use(express.json())
 
-app.get('/users', (req ,res) =>{
-
-    res.send("Hello")
-})
+app.use('/users', auth)
 
 app.listen(PORT, () => {
     console.log('server started')
